6f7c6ee71a3ba2333d6ad132dd3252a62186905e
bhaskar4k <bhaskarndy@gmail.com>
2025-05-19 14:25:48 +0530

    Updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,13 @@ import { ErrorComponent } from "./error/error.component";
 import { LogoutComponent } from "./logout/logout.component";
 import { LoginComponent } from "./login/login.component";
 import { LayoutComponent } from "./layout/layout.component";
+import { HomeComponent } from "./page-component/home/home.component";
 
 export const routes: Routes = [
-    // { path: '', component: HomeComponent },
-    // { path: 'home', component: HomeComponent },
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
     
     { path: 'login', component: LoginComponent },
-    { path: 'home', component: LayoutComponent },
     { path: 'logout', component: LogoutComponent },
     { path: '**', component: ErrorComponent },
 ];
