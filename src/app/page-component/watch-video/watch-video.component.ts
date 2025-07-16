6260b811d1c3a3850181c9835544efa624860b64
bhaskar4k <bhaskarndy@gmail.com>
2025-07-17 01:26:45 +0530

    Updated watch video

--- src/app/page-component/watch-video/watch-video.component.ts
+++ src/app/page-component/watch-video/watch-video.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-watch-video',
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './watch-video.component.css'
 })
 export class WatchVideoComponent {
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
+  guid: string = '';
+  playback: number = 0;
+
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe(params => {
+      this.guid = params.get('v') || '';
+      this.playback = Number(params.get('playback')) || 0;
+    });
+  }
 }
